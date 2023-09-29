e12a133eb8c252417968c3082066f97cd027f5d7
Patrik Cihal <patrik.cihal1234@gmail.com>
2023-09-29 14:52:23 +0200

    Recursive closures

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,3 +1,4 @@
 pub mod random;
 pub mod stdout;
-pub mod mim;
+pub mod minim_maxim;
+pub mod recursive_closure;
---
+++ src/misc/recursive_closure.rs
@@ -0,0 +1,54 @@
+use std::marker::PhantomData;
+
+macro_rules! recursive_closure {
+    ($name: ident, $trait: ident, ($($type: ident $arg: ident,)*)) => {
+        pub trait $trait<$($type, )*Output> {
+            fn call(&mut self, $($arg: $type,)*) -> Output;
+        }
+
+        pub struct $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            f: std::cell::UnsafeCell<F>,
+            $($arg: PhantomData<$type>,
+            )*
+            phantom_output: PhantomData<Output>,
+        }
+
+        impl<F, $($type, )*Output> $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            pub fn new(f: F) -> Self {
+                Self {
+                    f: std::cell::UnsafeCell::new(f),
+                    $($arg: Default::default(),
+                    )*
+                    phantom_output: Default::default(),
+                }
+            }
+        }
+
+        impl<F, $($type, )*Output> $trait<$($type, )*Output> for $name<F, $($type, )*Output>
+        where
+            F: FnMut(&mut dyn $trait<$($type, )*Output>, $($type, )*) -> Output,
+        {
+            fn call(&mut self, $($arg: $type,)*) -> Output {
+                unsafe { (*self.f.get())(self, $($arg, )*) }
+            }
+        }
+    }
+}
+
+
+recursive_closure!(RFn0, Callable0, ());
+recursive_closure!(RFn, Callable, (Arg arg,));
+recursive_closure!(RFn2, Callable2, (Arg1 arg1, Arg2 arg2,));
+recursive_closure!(RFn3, Callable3, (Arg1 arg1, Arg2 arg2, Arg3 arg3,));
+recursive_closure!(RFn4, Callable4, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4,));
+recursive_closure!(RFn5, Callable5, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5,));
+recursive_closure!(RFn6, Callable6, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6,));
+recursive_closure!(RFn7, Callable7, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7,));
+recursive_closure!(RFn8, Callable8, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8,));
+recursive_closure!(RFn9, Callable9, (Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9,));
