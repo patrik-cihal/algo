fceb59b2906f48adb2e649131471be39d81037cf
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-05-01 02:13:16 +0200

    IO

--- src/io.rs
+++ src/io.rs
@@ -1,30 +1,42 @@
-use std::io::{self, stdin, BufRead, BufWriter, StdinLock, Stdout, StdoutLock, Write};
+use std::{collections::VecDeque, io::{self, stdin, BufRead, BufWriter, StdinLock, Stdout, StdoutLock, Write}, str::{FromStr, SplitWhitespace}};
 
 pub static mut WRITER: Option<BufWriter<Stdout>> = None;
-pub static mut READER: Option<StdinLock> = None;
+pub static mut READER: Option<Reader<'static>> = None;
 
 pub fn writer<'a>() -> &'a mut BufWriter<Stdout> {
     unsafe {WRITER.get_or_insert_with(|| std::io::BufWriter::new(std::io::stdout()))}
 }
 
-pub fn reader<'a>() -> &'a mut StdinLock<'static> {
-    unsafe {READER.get_or_insert_with(|| stdin().lock())}
+pub fn reader<'a>() -> &'a mut Reader<'static> {
+    unsafe {READER.get_or_insert_with(|| Reader::<'static>::new())}
+}
+
+pub struct Reader<'a> {
+    reader: StdinLock<'a>,
+    active_line: VecDeque<String>
+}
+
+impl<'a> Reader<'a> {
+    pub fn new() -> Self {
+        Self {
+            reader: stdin().lock(),
+            active_line: VecDeque::new()
+        }
+    }
+    pub fn token<T: FromStr>(&mut self) -> T where <T as FromStr>::Err: std::fmt::Debug {
+        if let Some(token) = self.active_line.pop_front() {
+            return token.parse::<T>().unwrap();
+        }
+
+        let mut buf = String::new();
+        self.reader.read_line(&mut buf).unwrap();
+        self.active_line = buf.split_whitespace().map(|x| x.to_string()).collect();
+        self.token()
+    }
 }
 
 #[macro_export]
-macro_rules! outln {
-    // Match arguments similar to `println!`
-    () => (outln!(""));
-    ($fmt:expr) => ({
-        writeln!(writer(), $fmt).unwrap();
-    });
-    ($fmt:expr, $($arg:tt)*) => ({
-        writeln!(writer(), $fmt, $($arg)*).unwrap();
-    });
-}
-
-#[macro_export]
-macro_rules! out {
+macro_rules! outp {
     () => (out!(""));
     ($fmt:expr) => ({
         write!(writer(), $fmt).unwrap();
@@ -35,61 +47,46 @@ macro_rules! out {
 }
 
 #[macro_export]
-macro_rules! read {
-    () => {
-        reader().read_line(&mut String::new()).unwrap();
-    };
-    [$t : ty] => {{
-        let line = {
-            let mut buf = String::new();
-            reader().read_line(&mut buf).unwrap();
-            buf
-        };
-
-        line.split_whitespace().map(|x| x.parse::<$t>().unwrap()).collect::<Vec<_>>()
+macro_rules! inp {
+    [$t : ty; $len : expr] => {{
+        let reader = reader();
+        let mut res = Vec::with_capacity($len);
+        for _ in 0..$len {
+            res.push(reader.token::<$t>());
+        }
+        res
     }};
 
     ($($t:ty),+) => {{
-        let line = {
-            let mut buf = String::new();
-            reader().read_line(&mut buf).unwrap();
-            buf
-        };
+        let reader = reader();
 
-        let mut iter = line.split_whitespace();
         (
             $(
-                iter.next().expect("Expected more input").parse::<$t>()
-                    .expect(&format!("Failed to parse input as {}", stringify!($t)))
+                reader.token::<$t>()
             ),+
         )
     }};
 }
 
 
-#[cfg(test)]
 mod tests {
     use super::{writer, reader};
     use std::io::{Write, BufRead};
     #[test]
-    pub fn writing() {
-        outln!("Hello {}", "patrik");
-        writer().flush().unwrap();
+    pub fn test_output() {
+        outp!("Hello world\n");
+        outp!("{} And this bob\n", 54);
     }
     #[test]
-    pub fn read_write() {
-        out!("Your name please: ");
-        writer().flush().unwrap();
-    }
-    #[test]
-    pub fn reading_tuples() {
-        let n = read!(usize);
-        let v = read![usize];
+    pub fn test_input_and_output() {
+        let (n, k) = inp!(usize, usize);
+        let a = inp![usize; n];
 
-        for val in v {
-            out!("{val} ");
+        for val in a {
+            outp!("{val} ");
         }
-        outln!();
+        outp!("\n");
+
         writer().flush().unwrap();
     }
 }
